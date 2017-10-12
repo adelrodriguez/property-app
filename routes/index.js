6204e88897bca5e9cbe141b8ad35145d425df3f8
Adel Rodriguez <adelrodriguezv@gmail.com>
2017-10-12 18:10:12 -0400

    Add search bar

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const router = express.Router();
 
+// Require property model
+const Property = require('../models/property');
+
 router.get('/', (req, res) => {
   res.redirect('/propiedades');
+});
+
+router.get('/busqueda', (req, res) => {
+  // create a case-insensitive RegExp for search
+  let regexp = new RegExp(req.query.q, 'i');
+  // Search all properties that contain the regular expression
+  Property.find({
+    '$or': [
+      { 'description': regexp },
+      { 'city': regexp },
+      { 'province': regexp },
+      { 'country': regexp }
+    ]
+  }, (err, foundProperties) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('properties/index', {
+        title: "Propiedades encontradas",
+        properties: foundProperties
+      });
+    }
+  });
 });
 
 module.exports = router;
